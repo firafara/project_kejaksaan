95b623f371acfcbb1f980f6304105762d0a15ae9
firafara <106955105+firafara@users.noreply.github.com>
2024-07-16 21:24:59 +0700

    update API

--- lib/Api/Api.dart
+++ lib/Api/Api.dart
@@ -1,49 +1,72 @@
 class Api {
-  static String Koneksi             = "http://192.168.74.3/kejaksaan/koneksi.php";
-  static String Login               = "http://192.168.74.3/kejaksaan/login.php";
-  static String Register            = "http://192.168.74.3/kejaksaan/register.php";
-  static String AddJMS              = "http://192.168.74.3/kejaksaan/addjms.php";
-  static String AddPengaduan        = "http://192.168.74.3/kejaksaan/addpengaduan.php";
-  static String AddPengawasan       = "http://192.168.74.3/kejaksaan/addpengawasan.php";
-  static String AddPenyuluhan       = "http://192.168.74.3/kejaksaan/addpenyuluhan.php";
-  static String AddPidana           = "http://192.168.74.3/kejaksaan/addpidana.php";
-  static String AddPilkada          = "http://192.168.74.3/kejaksaan/addpilkada.php";
+  static String Koneksi = "https://umkm-pnp.com/api-kejaksaan/koneksi.php";
+  static String Login = "https://umkm-pnp.com/api-kejaksaan/login.php";
+  static String Register = "https://umkm-pnp.com/api-kejaksaan/register.php";
+  static String AddJMS = "https://umkm-pnp.com/api-kejaksaan/addjms.php";
+  static String AddPengaduan =
+      "https://umkm-pnp.com/api-kejaksaan/addpengaduan.php";
+  static String AddPengawasan =
+      "https://umkm-pnp.com/api-kejaksaan/addpengawasan.php";
+  static String AddPenyuluhan =
+      "https://umkm-pnp.com/api-kejaksaan/addpenyuluhan.php";
+  static String AddPidana = "https://umkm-pnp.com/api-kejaksaan/addpidana.php";
+  static String AddPilkada =
+      "https://umkm-pnp.com/api-kejaksaan/addpilkada.php";
 
-  static String DeleteJMS           = "http://192.168.74.3/kejaksaan/deletejms.php";
-  static String DeletePengaduan     = "http://192.168.74.3/kejaksaan/deletepengaduan.php";
-  static String DeletePengawasan    = "http://192.168.74.3/kejaksaan/deletepengawasan.php";
-  static String DeletePenyuluhan    = "http://192.168.74.3/kejaksaan/deletepenyuluhan.php";
-  static String DeletePidana        = "http://192.168.74.3/kejaksaan/deletepidana.php";
-  static String DeletePilkada       = "http://192.168.74.3/kejaksaan/deletepilkada.php";
+  static String DeleteJMS = "https://umkm-pnp.com/api-kejaksaan/deletejms.php";
+  static String DeletePengaduan =
+      "https://umkm-pnp.com/api-kejaksaan/deletepengaduan.php";
+  static String DeletePengawasan =
+      "https://umkm-pnp.com/api-kejaksaan/deletepengawasan.php";
+  static String DeletePenyuluhan =
+      "https://umkm-pnp.com/api-kejaksaan/deletepenyuluhan.php";
+  static String DeletePidana =
+      "https://umkm-pnp.com/api-kejaksaan/deletepidana.php";
+  static String DeletePilkada =
+      "https://umkm-pnp.com/api-kejaksaan/deletepilkada.php";
 
-  static String EditJMS             = "http://192.168.74.3/kejaksaan/editjms.php";
-  static String EditPengaduan       = "http://192.168.74.3/kejaksaan/editpengaduan.php";
-  static String EditPengawasan      = "http://192.168.74.3/kejaksaan/editpengawasan.php";
-  static String EditPenyuluhan      = "http://192.168.74.3/kejaksaan/editpenyuluhan.php";
-  static String EditPidana          = "http://192.168.74.3/kejaksaan/editpidana.php";
-  static String EditPilkada         = "http://192.168.74.3/kejaksaan/editpilkada.php";
+  static String EditJMS = "https://umkm-pnp.com/api-kejaksaan/editjms.php";
+  static String EditPengaduan =
+      "https://umkm-pnp.com/api-kejaksaan/editpengaduan.php";
+  static String EditPengawasan =
+      "https://umkm-pnp.com/api-kejaksaan/editpengawasan.php";
+  static String EditPenyuluhan =
+      "https://umkm-pnp.com/api-kejaksaan/editpenyuluhan.php";
+  static String EditPidana =
+      "https://umkm-pnp.com/api-kejaksaan/editpidana.php";
+  static String EditPilkada =
+      "https://umkm-pnp.com/api-kejaksaan/editpilkada.php";
 
-  static String JMS                 = "http://192.168.74.3/kejaksaan/jms.php";
-  static String Pengaduan           = "http://192.168.74.3/kejaksaan/pengaduan.php";
-  static String Pengawasan          = "http://192.168.74.3/kejaksaan/pengawasan.php";
-  static String Penyuluhan          = "http://192.168.74.3/kejaksaan/penyuluhan.php";
-  static String Pidana              = "http://192.168.74.3/kejaksaan/pidana.php";
-  static String Pilkada             = "http://192.168.74.3/kejaksaan/pilkada.php";
+  static String JMS = "https://umkm-pnp.com/api-kejaksaan/jms.php";
+  static String Pengaduan = "https://umkm-pnp.com/api-kejaksaan/pengaduan.php";
+  static String Pengawasan =
+      "https://umkm-pnp.com/api-kejaksaan/pengawasan.php";
+  static String Penyuluhan =
+      "https://umkm-pnp.com/api-kejaksaan/penyuluhan.php";
+  static String Pidana = "https://umkm-pnp.com/api-kejaksaan/pidana.php";
+  static String Pilkada = "https://umkm-pnp.com/api-kejaksaan/pilkada.php";
 
-  static String UpdateStatusJMS           = "http://192.168.74.3/kejaksaan/updateStatusJms.php";
-  static String UpdateStatusPengaduan     = "http://192.168.74.3/kejaksaan/updateStatusPengaduan.php";
-  static String UpdateStatusPengawasan    = "http://192.168.74.3/kejaksaan/updateStatusPengawasan.php";
-  static String UpdateStatusPenyuluhan    = "http://192.168.74.3/kejaksaan/updateStatusPenyuluhan.php";
-  static String UpdateStatusPidana        = "http://192.168.74.3/kejaksaan/updateStatusPidana.php";
-  static String UpdateStatusPilkada       = "http://192.168.74.3/kejaksaan/updateStatusPilkada.php";
+  static String UpdateStatusJMS =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusJms.php";
+  static String UpdateStatusPengaduan =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusPengaduan.php";
+  static String UpdateStatusPengawasan =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusPengawasan.php";
+  static String UpdateStatusPenyuluhan =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusPenyuluhan.php";
+  static String UpdateStatusPidana =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusPidana.php";
+  static String UpdateStatusPilkada =
+      "https://umkm-pnp.com/api-kejaksaan/updateStatusPilkada.php";
 
-  static String AddRating           = "http://192.168.74.3/kejaksaan/addrating.php";
-  static String UpdateUser          = "http://192.168.74.3/kejaksaan/updateUser.php";
-  static String User                = "http://192.168.74.3/kejaksaan/user.php";
-  static String GetUser                = "http://192.168.74.3/kejaksaan/getUser.php";
-  static String LogApp              = "http://192.168.74.3/kejaksaan/logapp.php";
-  static String GetLog                = "http://192.168.74.3/kejaksaan/getLog.php";
-  static String Rating              = "http://192.168.74.3/kejaksaan/rating.php";
-  static String Upload              = "http://192.168.74.3/kejaksaan/upload.php";
-  static String Uploads             = "http://192.168.74.3/kejaksaan/uploads/";
+  static String AddRating = "https://umkm-pnp.com/api-kejaksaan/addrating.php";
+  static String UpdateUser =
+      "https://umkm-pnp.com/api-kejaksaan/updateUser.php";
+  static String User = "https://umkm-pnp.com/api-kejaksaan/user.php";
+  static String GetUser = "https://umkm-pnp.com/api-kejaksaan/getUser.php";
+  static String LogApp = "https://umkm-pnp.com/api-kejaksaan/logapp.php";
+  static String GetLog = "https://umkm-pnp.com/api-kejaksaan/getLog.php";
+  static String Rating = "https://umkm-pnp.com/api-kejaksaan/rating.php";
+  static String Upload = "https://umkm-pnp.com/api-kejaksaan/upload.php";
+  static String Uploads = "https://umkm-pnp.com/api-kejaksaan/uploads/";
 }
